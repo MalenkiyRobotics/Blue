abf384102f90903bd8458c4580e3e2a537706eb0
Aitchbar <gergely06@gmail.com>
2017-11-29 15:47:39 +0100

    Reading args
    .

---
+++ args.f
@@ -0,0 +1,15 @@
+PROGRAM MAIN  
+     REAL(8)    :: A,B
+     integer :: num_args, ix
+     character(len=12), dimension(:), allocatable :: args
+
+     num_args = command_argument_count()
+     allocate(args(num_args))  ! I've omitted checking the return status of the allocation 
+
+     do ix = 1, num_args
+         call get_command_argument(ix,args(ix))
+         ! now parse the argument as you wish
+     end do
+
+     PRINT*, A+B, COMMAND_ARGUMENT_COUNT()
+END PROGRAM MAIN
